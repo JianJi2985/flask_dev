8752cec9434cd2d8718659dfe791ec8659c32950
JianJi2985 <44628374+JianJi2985@users.noreply.github.com>
2020-12-05 23:10:04 +0800

    modify

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify, redirect, url_for, render_template,flash
+from flask import Flask, request, jsonify, redirect, url_for, render_template, flash
 from flask_script import Manager, Shell
+from flask.views import MethodView
 from flask_mail import Mail, Message
 from threading import Thread
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +11,7 @@ import uuid
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, ValidationError, Length
+from time import sleep
 
 import os
 
@@ -33,10 +35,12 @@ login_manager = LoginManager()  # 实例化登录管理对象
 login_manager.init_app(app)  # 初始化应用
 login_manager.login_view = 'login'  # 设置用户登录视图函数 endpoint
 
+
 #     =False,
 
 class RegisterForm(FlaskForm):
     """注册表单类"""
+
     def __init__(self, *args, **kwargs):
         kwargs['csrf_enabled'] = False
         super(RegisterForm, self).__init__(*args, **kwargs)
@@ -50,8 +54,9 @@ class RegisterForm(FlaskForm):
             raise ValidationError('Username %s already exists!' % field.data)
 
     def validate_password(self, field):
-        if self.password!=self.confirm:
+        if self.password != self.confirm:
             raise ValidationError('Password inconsistency!')
+
 
 class LoginForm(FlaskForm):
     """登录表单类"""
@@ -73,9 +78,7 @@ class LoginForm(FlaskForm):
         if not self.get_user():
             return
         if not self.get_user().verify_password(field.data):
-
             raise ValidationError('Incorrect password!')
-
 
 
 class User(UserMixin, db.Model):
@@ -118,12 +121,11 @@ def send_email():
     return jsonify(dict(msg="Success"))
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     user_data = request.get_json()
     form = RegisterForm(data=user_data)
     if form.validate():
-
         user = User(username=user_data['username'], password=user_data['password'])
         print(user.id)
         db.session.add(user)
@@ -134,20 +136,8 @@ def register():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    user_data=request.get_json()
-    # print('user_Data next')
-    # print(user_data)
-    # print(request.form)
-    # print('user_data end')
+    user_data = request.get_json()
     form = LoginForm(data=user_data)
-    # print(form.username.data)
-    # print(form.password.data)
-    # print('is validate_username ')
-    # print(form.validate_username(form.username))
-    # print('is validate_password ')
-    # print(form.validate_password(form.password))
-    # print('is vali',form.validate())
-    # print('form error ')
 
     emsg = None
     if form.validate():
@@ -170,6 +160,77 @@ def index():
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+@app.route('/long-polling')
+def long_polling():
+    i = 0
+    while True:
+        if i == 10:
+            return jsonify({'error': 0,'msg':'未查询到更新'})
+        else:
+            i += 1
+            sleep(5)
+# CREATE TABLE Trade (
+#     ID int NOT NULL,
+#     product varchar(50) NOT NULL,
+#     volumn float not null,
+#     timestamp date,
+#     is_valid boolean,
+#     PRIMARY KEY (ID)
+# );
+# insert into trade value(1,'A',5.4,'2020-01-02',True);
+class Trade(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    product = db.Column(db.String(50),nullable=False)
+    volumn = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    is_valid = db.Column(db.Boolean, nullable=False)
+
+    def __init__(self, **kwargs):
+        super(Trade, self).__init__(**kwargs)
+
+class TradeAPI(MethodView):
+
+    def get(self):
+        trade=request.get_json()
+        if ('start_date' not in trade) or (not trade['start_date']):
+            start_date='2000-01-01'
+        if ('end_date' not in trade) or (not trade['end_date']):
+            end_date='2020-12-31'
+
+        result=db.session.query('Trade').filter(Trade.timestamp.between(start_date, end_date)).all()
+        return jsonify(result)
+
+    def post(self):
+        trade = request.get_json()
+        new_trade= Trade(trade)
+        db.session.add(new_trade)
+        db.session.commit()
+        return jsonify({'status':'success'})
+
+
+    # @login_required # need to implement
+    # @admin_required
+    def delete(self):
+        id=request.get_json()['id']
+        Trade.query.filter_by(id=id).delete()
+        db.commit()
+        return jsonify({'status':'success'})
+
+
+    def put(self):
+        trade = request.get_json()
+        current_trade=Trade.query.get(trade['id'])
+        current_trade.product=trade['product']
+        current_trade.volumn=trade['volumn']
+        current_trade.timestamp = trade['timestamp']
+        current_trade.is_valid = trade['is_valid']
+        db.session.commit()
+        return jsonify({'status':'success'})
+
+trade_view = TradeAPI.as_view('trade_api')
+app.add_url_rule('/trade/',view_func=trade_view, methods=['GET','POST','PUT','DELETE'])
 
 
 if __name__ == '__main__':
