413d753eea6ec4397b030a9df3bd72dbd7862023
JianJi2985 <44628374+JianJi2985@users.noreply.github.com>
2020-12-17 00:10:43 +0800

    update

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ from time import sleep
 from datetime import datetime
 from flask_socketio import SocketIO
 import tushare as ts
-from models import db, User
+from models import db, User, BondFlag
 import os
-from blueprint import auth,bonds,echarts
+from blueprint import auth, bonds, echarts
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_socketio import SocketIO
+
 
 # app = Flask(__name__)
 # app.secret_key = 'iashdfpi'
@@ -57,79 +58,25 @@ from flask_socketio import SocketIO
 # login_manager.login_view = 'login'  # 设置用户登录视图函数 endpoint
 
 
-def interval_test():
-    print('interval job')
 
 
-def job_test(a, b):
+
+def daily_fetch_data():
     # stock_all = ts.get_today_all()
     stock_all = ts.get_k_data(code='600000', start='2019-10-01', end='2019-10-07')
     stock_all.to_csv('./download/%s.csv' % (datetime.today().strftime('%Y%m%d')))
-    print('job_test ', a + b)
-
-
-# class RegisterForm(FlaskForm):
-#     """注册表单类"""
-#
-#     def __init__(self, *args, **kwargs):
-#         kwargs['csrf_enabled'] = False
-#         super(RegisterForm, self).__init__(*args, **kwargs)
-#
-#     username = StringField('Username', validators=[Length(max=64)])
-#     password = PasswordField('Password', validators=[Length(8, 16)])
-#     confirm = PasswordField('Confirm Password')
-#
-#     def validate_username(self, field):
-#         if User.query.filter_by(username=field.data).count() > 0:
-#             raise ValidationError('Username %s already exists!' % field.data)
-#
-#     def validate_password(self, field):
-#         if self.password != self.confirm:
-#             raise ValidationError('Password inconsistency!')
-
-
-# class LoginForm(FlaskForm):
-#     """登录表单类"""
-#     username = StringField('username', validators=[DataRequired()])
-#     password = PasswordField('password', validators=[DataRequired()])
-#
-#     def __init__(self, *args, **kwargs):
-#         kwargs['csrf_enabled'] = False
-#         super(LoginForm, self).__init__(*args, **kwargs)
-#
-#     def get_user(self):
-#         return User.query.filter_by(username=self.username.data).first()
-#
-#     def validate_username(self, field):
-#         if not self.get_user():
-#             raise ValidationError('Invalid username!')
-#
-#     def validate_password(self, field):
-#         if not self.get_user():
-#             return
-#         if not self.get_user().verify_password(field.data):
-#             raise ValidationError('Incorrect password!')
-
-
-# class User(UserMixin, db.Model):
-#     """用户类"""
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(200), unique=True)
-#     password = db.Column(db.String(200))
-#
-#     def __init__(self, **kwarg):
-#         kwarg['password'] = generate_password_hash(kwarg.get("password"))
-#         super(User, self).__init__(**kwarg)
-#
-#     def verify_password(self, password):
-#         """密码验证"""
-#         if self.password is None:
-#             return False
-#         return check_password_hash(self.password, password)
+    filename = datetime.today().strftime('%Y%m%d')
+    id = BondFlag.query.count() + 1
+    newBondFlag = BondFlag(id=id, filename=filename, isvalid=True)
+    db.session.add(newBondFlag)
+    db.session.commit()
+    print('daily_fetch_data success ')
 
 
 msg = Message('test', sender='rbs_jijian@163.com', recipients=['rbs_jijian@163.com'])
 msg.body = "This is a first email"
+
+
 # with app.open_resource("./test_mail_attachment.txt") as fp:
 #     msg.attach("test_mail_attachment.txt", "text/plain", fp.read())
 
@@ -195,68 +142,14 @@ def send_async_email(app, msg):
 #         else:
 #             i += 1
 #             sleep(5)
+# def interval_test():
 
-
-class Trade(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    product = db.Column(db.String(50), nullable=False)
-    volumn = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
-    is_valid = db.Column(db.Boolean, nullable=False)
-
-    def __init__(self, **kwargs):
-        super(Trade, self).__init__(**kwargs)
-
-
-class TradeAPI(MethodView):
-    # __tablename__='Trade'
-    def get(self):
-        trade = request.args.to_dict()
-        print(trade)
-        if ('start_date' not in trade) or (not trade['start_date']):
-            start_date = '2000-01-01'
-        else:
-            start_date = trade['start_date']
-        if ('end_date' not in trade) or (not trade['end_date']):
-            end_date = '2020-12-31'
-        else:
-            end_date = trade['end_date']
-        result = ""
-        result = db.session.query('id').filter(Trade.timestamp.between(start_date, end_date)).all()
-        return jsonify(result)
-
-    def post(self):
-        trade = request.get_json()
-        new_trade = Trade(trade)
-        db.session.add(new_trade)
-        db.session.commit()
-        return jsonify({'status': 'success'})
-
-    # @login_required # need to implement
-    # @admin_required
-    def delete(self):
-        id = request.get_json()['id']
-        Trade.query.filter_by(id=id).delete()
-        db.commit()
-        return jsonify({'status': 'success'})
-
-    def put(self):
-        trade = request.get_json()
-        current_trade = Trade.query.get(trade['id'])
-        current_trade.product = trade['product']
-        current_trade.volumn = trade['volumn']
-        current_trade.timestamp = trade['timestamp']
-        current_trade.is_valid = trade['is_valid']
-        db.session.commit()
-        return jsonify({'status': 'success'})
-
-
-trade_view = TradeAPI.as_view('trade_api')
-# app.add_url_rule('/trade/', view_func=trade_view, methods=['GET', 'POST', 'PUT', 'DELETE'])
 
 
 def create_app():
+
     app = Flask(__name__)
+
     app.secret_key = 'iashdfpi'
     app.config.update(
         SCHEDULER_API_ENABLED=True,
@@ -273,15 +166,15 @@ def create_app():
         MAIL_DEBUG=True
     )
     app.config['JOBS'] = [{'id': 'job1',
-                           'func': '__main__:job_test',
-                           'args': (1, 2),
+                           'func': '__main__:daily_fetch_data',
+                           # 'args': (1, 2),
                            'trigger': 'cron',  # cron表示定时任务
                            'hour': 20,
                            'minute': 9},
                           # {'id': 'job2',
                           #  'func': '__main__:interval_test',
                           #  'trigger': 'interval',
-                          #  'seconds': 20,
+                          #  'seconds': 5,
                           #  }
                           ]
     mail = Mail(app)
@@ -301,6 +194,7 @@ def create_app():
     scheduler = APScheduler()
     scheduler.init_app(app)
     scheduler.start()
+
     # socketio = SocketIO(app, cors_allowed_origins='*')
 
     @login_manager.user_loader
@@ -311,7 +205,6 @@ def create_app():
     @login_required  # 需要登录才能访问
     def index():
         return render_template('index.html')
-
 
     app.register_blueprint(auth.bp, url_prefix="/auth")
     app.register_blueprint(bonds.bp, url_prefix="/rule")
